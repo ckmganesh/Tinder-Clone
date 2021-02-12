313ce39f93b0b41024bf2358f47c56e7ca3f223c
ckmganesh <ckmganesh2000@gmail.com>
2021-02-13 00:04:00 +0530

    Basic Setup Stuff

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Header from "./Header";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+    <h1>Let's build the MERN TINDER clone</h1>
+    <Header/>
+    {/*Header*/}
+    {/*Tinder Cards*/}
+    {/*SwipeCars*/}
     </div>
   );
 }
---
+++ src/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Header() {
+    return (
+        <div className="header">
+            <h2>I am header</h2>
+            
+        </div>
+    )
+}
+
+export default Header
