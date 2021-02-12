3492c8ffc83760591be667a1e2c0c15e4e345d70
ckmganesh <ckmganesh2000@gmail.com>
2021-02-13 00:49:57 +0530

    Navbar started

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import Header from "./Header";
 function App() {
   return (
     <div className="app">
-    <h1>Let's build the MERN TINDER clone</h1>
     <Header/>
     {/*Header*/}
     {/*Tinder Cards*/}
--- src/Header.js
+++ src/Header.js
@@ -1,12 +1,21 @@
 import React from 'react'
+import './Header.css';
+import PersonIcon from '@material-ui/icons/Person';
+import IconButton from '@material-ui/core/IconButton';
 
 function Header() {
     return (
         <div className="header">
-            <h2>I am header</h2>
+            <IconButton>
+            <PersonIcon fontSize = "large" className ="header__icon"/>
+            </IconButton>
+
+            <img className="header__logo" src="https://www.globaldatinginsights.com/wp-content/uploads/2017/08/Screen-Shot-2017-08-17-at-10.04.531.png" alt=""/>
+
+            
             
         </div>
-    )
+    );
 }
 
-export default Header
+export default Header;
