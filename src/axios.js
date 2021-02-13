77b40396c4e220d4083e5d0c214b3fb3f11d85a7
ckmganesh <ckmganesh2000@gmail.com>
2021-02-14 02:09:27 +0530

    Final touches

--- src/axios.js
+++ src/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 const instance = axios.create({
-    baseURL:'http://localhost:8001'
+    baseURL:'https://tinder-backend-clone-ckm.herokuapp.com'
 });
 
 export default instance;
